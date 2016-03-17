c5634d353b84a98b44c21f78e134fe0f1bc78044
emilkaumov <emil.kayumov@gmail.com>
2016-03-17 13:51:56 +0300

    added all code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # kaggle-dota2-win-prediction
 Kaggle competition Dota 2: Win Probability Prediction for CMC MSU ML class.
+
+https://inclass.kaggle.com/c/cmc-msu-machine-learning-spring-2015-2016-dota-competition
+
+An ensemble of two Vowpal Wabbit.
+
+AUC: 0.76458.
---
+++ extract_abilities.py
@@ -0,0 +1,78 @@
+import bz2
+import json
+import pandas
+import collections
+import argparse
+import numpy
+
+
+def last_value(series, times, time_point=60*5):
+    values = [v for t, v in zip(times, series) if t <= time_point]
+    return values[-1] if len(values) > 0 else 0
+
+def filter_events(events, time_point=60*5):
+    return [event for event in events if event['time'] <= time_point]
+
+def extract_match_features(match, time_point=None):
+
+    abilities = pandas.read_csv('../input/dictionaries/abilities.csv', header=0)
+    abilities = numpy.array(abilities)
+
+    extract_abilities_count = [(x[0], x[1]) for x in abilities]
+
+    feats = [
+        ('match_id', match['match_id'])
+    ]
+
+    # team features
+    radiant_players = match['players'][:5]
+    dire_players = match['players'][5:]
+
+    for team, team_players in (('radiant', radiant_players), ('dire', dire_players)):
+
+        for ability_id, ability_name in extract_abilities_count:
+            abilities_count = sum([
+                1
+                for player in team_players
+                for entry in filter_events(player['ability_upgrades'], time_point)
+                if entry['ability'] == ability_id
+            ])
+            feats += [
+                ('%s_%s_count' % (team, ability_name), abilities_count)
+            ]
+
+    return collections.OrderedDict(feats)
+
+
+def iterate_matches(matches_filename):
+    with bz2.BZ2File(matches_filename) as f:
+        for n, line in enumerate(f):
+            match = json.loads(line)
+            yield match
+            if (n+1) % 1000 == 0:
+                print 'Processed %d matches' % (n+1)
+
+
+def create_table(matches_filename, time_point):
+    df = {}
+    fields = None
+    for match in iterate_matches(matches_filename):
+        features = extract_match_features(match, time_point)
+        if fields is None:
+            fields = features.keys()
+            df = {key: [] for key in fields}
+        for key, value in features.iteritems():
+            df[key].append(value)
+    df = pandas.DataFrame.from_records(df).ix[:, fields].set_index('match_id').sort_index()
+    return df
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Extract features from matches data')
+    parser.add_argument('input_matches')
+    parser.add_argument('output_csv')
+    parser.add_argument('--time', type=int, default=5*60)
+    args = parser.parse_args()
+
+    features_table = create_table(args.input_matches, args.time)
+    features_table.to_csv(args.output_csv)
